e217a7e71961e79698a866823bcc5ce2226aa6da
geovannysan <geovanny1santiago@gmail.com>
2023-04-21 17:20:31 -0500

    agregando dasts

--- Program.cs
+++ Program.cs
@@ -1,30 +1,55 @@
 using Microsoft.EntityFrameworkCore;
 using Backrest.Models;
+using DotNetEnv;
+using Microsoft.Extensions.Configuration;
 using Backrest.Data;
 using System.Text.Json.Serialization;
 using MySql.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+Env.Load(); // carga las variables de entorno desde el archivo .env
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+var connectionString = builder.Configuration.GetConnectionString("Dataconnetion");
+connectionString = connectionString.Replace("{DB_HOST}", Environment.GetEnvironmentVariable("DB_HOST"))
+    .Replace("{DB_PORT}", Environment.GetEnvironmentVariable("DB_PORT"))
+    .Replace("{DB_NAME}", Environment.GetEnvironmentVariable("DB_NAME"))    
+    .Replace("{DB_USER}", Environment.GetEnvironmentVariable("DB_USER"));
 
 // Add services to the container.
 builder.Services.AddHttpClient();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//conecion sql
-builder.Services.AddDbContext<DataContext>(opt=>opt.UseMySQL(builder.Configuration.GetConnectionString("Dataconnetion")));
 
+//conecion sql
+builder.Services.AddDbContext<DataContext>(
+    opt => opt.UseMySQL(connectionString)
+);
 
 //eliminar referencias ciclicas
-builder.Services.AddControllers().AddJsonOptions(opt=>{
-opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-});
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(opt =>
+    {
+        opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
+
 //cors del proyecto
 var reglas = "CorsReglas";
-builder.Services.AddCors(opt =>{
-opt.AddPolicy(name: reglas,builder=>{
-    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
-});
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(
+        name: reglas,
+        builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        }
+    );
 });
 var app = builder.Build();
 
@@ -34,8 +59,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }*/
- app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseCors(reglas);
 app.UseHttpsRedirection();
 
